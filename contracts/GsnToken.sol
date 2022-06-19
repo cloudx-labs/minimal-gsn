7eb6baf67bebbe58dec431e8216fbb9e8ca256fa
Pablo Coronel <me@pablitxn.io>
2022-06-18 23:35:31 -0300

    feat: add gitattributes for solidity

--- contracts/GsnToken.sol
+++ contracts/GsnToken.sol
@@ -2,34 +2,17 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GsnToken is ERC20, ERC20Burnable, Pausable, Ownable {
+contract GsnToken is ERC20, Ownable {
     uint8 private _decimals = 0;
 
     constructor() ERC20("GsnToken", "$GSN") {
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
+        _mint(msg.sender, 10000);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, amount);
     }
 
     function decimals() public view virtual override returns (uint8) {
