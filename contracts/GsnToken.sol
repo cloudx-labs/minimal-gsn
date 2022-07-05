e420b5a98a81cf4b944c3a958374ea885b0e6a5b
Pablo Coronel <me@pablitxn.io>
2022-07-05 17:57:26 -0300

    debugging

--- contracts/GsnToken.sol
+++ contracts/GsnToken.sol
@@ -10,6 +10,7 @@ contract GsnToken is ERC20, Ownable {
     constructor(address stakeManager) ERC20("GsnToken", "$GSN") {
         _mint(msg.sender, 10000);
         _mint(stakeManager, 10000);
+        _approve(msg.sender, stakeManager, 10000);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
